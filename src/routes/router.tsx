ba48b53d32a078818a5fde2472322f5f8f293775
Caio Braga <caioaugustobrg@gmail.com>
2024-01-19 03:02:07 -0300

    :sparkles: feat: login route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,13 +5,38 @@ import {
 	Routes
 } from 'react-router-dom';
 import Home from '../pages/home';
+import Login from '../pages/Login';
+import Header from '../components/Header';
 const AppRouter = () => {
     return (
         <Router>
+              <Header />
             <Routes>
             <Route path='/' element={<Home /> } />
+            <Route path='/login' element={<Login />}/>
             </Routes>
         </Router>
     )
 }
 export default AppRouter;
+{/* <Input
+              backgroundColor={"#1B1C1D"}
+              borderRadius={6}
+              w={320}
+              h={40}
+              m={10}
+              p={10}
+              placeholder="Email"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p
+                style={{
+                  color: "red",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              >
+                {errors.email.message}
+              </p>
+            )} */}
